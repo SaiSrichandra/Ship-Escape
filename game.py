80258fc345c8f6f4c60c366187195082d4694679
SaiSrichandra <53914157+SaiSrichandra@users.noreply.github.com>
2023-10-03 21:30:11 -0400

    Add files via upload

--- game.py
+++ game.py
@@ -3,27 +3,28 @@ import random
 import time
 import copy
 from heapq import *
+import sys
 
 Bot = 37
 Alien = 41
 Crew = 49
 
-def print_ship():
-    print(ship)
+def print_ship(f = sys.stdout):
+    print(ship,file=f)
     # print()
     return
 
 def import_ship(loc):
+    global ship
     ship = np.loadtxt(str(loc), dtype = int)
     global dim
     dim = ship.shape[0]
     print(dim)
     ship = np.matrix(ship)
     print("Ship Layout Imported")
-    return ship
 
 def place_pieces(bot_id, alien_id, crew_id, k):
-    
+    global ship
     open_cells = np.where(ship[:,:] == 1)
     open_cells = list(zip(list(open_cells[0]),list(open_cells[1])))
     piece_pos = random.sample(open_cells, k+2)
@@ -47,7 +48,7 @@ def place_pieces(bot_id, alien_id, crew_id, k):
     return
 
 def replace_crew(prev_res):
-    global crew_pos, bot_pos
+    global crew_pos, bot_pos,ship
     if prev_res == -1:
         ship[crew_pos[0],crew_pos[1]] = 1
     else:
@@ -66,7 +67,7 @@ def replace_crew(prev_res):
 
 
 def valid_alien_moves(pos_t):
-    global dim
+    global dim, ship
     d = dim
     posX, posY = pos_t
     valid_list = []
@@ -82,7 +83,7 @@ def valid_alien_moves(pos_t):
     return valid_list
 
 def move_aliens():
-    global alien_pos, bot_pos, crew_pos
+    global alien_pos, bot_pos, crew_pos,ship
     random.shuffle(alien_pos)
     for i in range(len(alien_pos)):
         val_moves = valid_alien_moves(alien_pos[i])
@@ -90,7 +91,7 @@ def move_aliens():
             continue
         new_alien_pos = (random.choice(val_moves))
         if tuple(new_alien_pos) in [bot_pos]:
-            print("Byeeeeeeeeeeee")
+            print("Alien Caught meeeeeeeeeeeee")
             exit(0)
         if tuple(alien_pos[i]) not in [crew_pos]:
             ship[alien_pos[i][0], alien_pos[i][1]] = 1
@@ -100,7 +101,7 @@ def move_aliens():
 
 
 def valid_neighbours(posX,posY):
-    global dim
+    global dim,ship
     d = dim
     valid_list = []
     if posX+1 < d and ship[posX+1,posY] in [1,49]:
@@ -120,7 +121,7 @@ def manhattan_dist(posX,posY):
 
 
 def path_trace_algo(parents, curr_cell):
-    global bot_pos
+    global bot_pos,ship
     path = []
     posX,posY = curr_cell
     while posX != -1 and posY != -1:
@@ -129,9 +130,9 @@ def path_trace_algo(parents, curr_cell):
     return path[::-1]
 
 def A_star_algo():
-    global bot_pos, crew_pos
-    print(bot_pos)
-    print(crew_pos)
+    global bot_pos, crew_pos,ship
+    # print(bot_pos)
+    # print(crew_pos)
     visited = []
     parents = [[(-1,-1,0) for i in range(dim)] for i in range(dim)]
     heap = []
@@ -139,7 +140,7 @@ def A_star_algo():
     visited.append(bot_pos)
     while heap:
         curr_cell = heappop(heap)
-        print(curr_cell[1])
+        # print(curr_cell[1])
         if curr_cell[1] in [crew_pos]:
             return path_trace_algo(parents,curr_cell[1])
         valid_next_cells = valid_neighbours(curr_cell[1][0],curr_cell[1][1])
@@ -151,23 +152,23 @@ def A_star_algo():
             heappush(heap,(cell_heuristic + cell_g, tuple(cell)))
             parents[cell[0]][cell[1]] = (curr_cell[1][0], curr_cell[1][1], cell_g)
             
-            for i in range(0,dim):
-                for j in range(0,dim):
-                    print(str(parents[i][j]), end=" ")
-                print("\n")
-            print("\n\n")
+            # for i in range(0,dim):
+            #     for j in range(0,dim):
+            #         print(str(parents[i][j]), end=" ")
+            #     print("\n")
+            # print("\n\n")
 
             visited.append(cell)
-    print("No path is possible")
+    # print("No path is possible")
 
 
 
 
 
-ship = import_ship("ship_test.txt") 
-place_pieces(Bot, Alien, Crew, 3)
-print_ship()
-print(A_star_algo())
+# import_ship("ship_layout.txt") 
+# place_pieces(Bot, Alien, Crew, 3)
+# print_ship()
+# print(A_star_algo())
 # backup = ship.shape[0] + 1
 # while True:
 #     time.sleep(1)
